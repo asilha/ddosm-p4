78e8f8f66cdb7abedf05616c63757db36ce183e8
Alexandre Ilha <44216548+asilha@users.noreply.github.com>
2019-05-30 19:23:20 -0300

    Modified paths to point to local p4c and simple_switch binaries. This is a workaround.

--- scripts/run.sh
+++ scripts/run.sh
@@ -14,11 +14,13 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 $SCRIPT_DIR/veth.sh setup 6
 
+SS_PREFIX="../../aclapolli-bmv2/targets/simple_switch"
+
 trap sigint_handler SIGINT
-simple_switch -i 0@veth0 -i 1@veth2 -i 2@veth4 $SCRIPT_DIR/../build/ddosd.json &
+$SS_PREFIX/simple_switch -i 0@veth0 -i 1@veth2 -i 2@veth4 $SCRIPT_DIR/../build/ddosd.json &
 pid=$!
-sleep 1
-simple_switch_CLI < $SCRIPT_DIR/control_rules.txt
+sleep 15
+$SS_PREFIX/simple_switch_CLI < $SCRIPT_DIR/control_rules.txt
 wait $pid
 
 $SCRIPT_DIR/veth.sh delete 6
