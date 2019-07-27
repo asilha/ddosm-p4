484b12a5314c76337ec6bd289f432437d817d0c9
Alexandre Ilha <44216548+asilha@users.noreply.github.com>
2019-07-27 19:29:23 -0300

    Major improvements

--- scripts/sniff.py
+++ scripts/sniff.py
@@ -5,16 +5,34 @@
 import sys
 import struct
 
-from scapy.all import sniff, sendp, hexdump, get_if_list, get_if_hwaddr
-from scapy.all import Packet, IPOption
-from scapy.all import PacketListField, ShortField, IntField, LongField, BitField, FieldListField, FieldLenField
-from scapy.all import IP, UDP, Raw
-from scapy.layers.inet import _IPOption_HDR
+from scapy.all import bind_layers, hexdump, sniff
+from scapy.all import Packet, Ether, IP, TCP, UDP, Raw
+from scapy.all import ByteField, XShortField, IntField, LongField
+
+class DDoSD(Packet):
+    name = "DDoSD Header"
+    fields_desc = [IntField('pkt_num',0),
+    IntField('src_ent',0),
+    IntField('src_ewma',0),
+    IntField('src_ewmmd',0),
+    IntField('dst_ent',0),
+    IntField('dst_ewma',0),
+    IntField('dst_ewmmd',0), 
+    ByteField('alarm',0), 
+    ByteField('defcon',0), 
+    XShortField('ethertype',0)]
+
+    def mysummary(self):
+        print('src_ent', self.getfieldval('src_ent')/16.0)
+
+class DDoSDPayload(Packet):
+    name = "DDoSD Payload"
+    fields_desc = [LongField('ts_sec',0), 
+    LongField('ts_usec',0), 
+    ByteField('is_attack',0)]
 
 def handle_pkt(pkt):
-    print('got a packet')
-    pkt.show2()
-    hexdump(pkt)
+    pkt.show()
     sys.stdout.flush()
 
 
@@ -22,6 +40,11 @@ def main():
     iface = 'veth7'
     print("sniffing on %s" % iface)
     sys.stdout.flush()
+
+    bind_layers(Ether, DDoSD, type=0x6605)
+    bind_layers(DDoSD, IP, ethertype=0x0800)
+    bind_layers(IP, DDoSDPayload, proto=253)
+
     sniff(filter='', iface = iface, prn = lambda x: handle_pkt(x))
 
 if __name__ == '__main__':
