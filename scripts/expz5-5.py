7460e02f3a5a3e47f7bff204a45df657d675f280
Alexandre Ilha <44216548+asilha@users.noreply.github.com>
2019-08-04 14:32:05 -0300

    Updated and organized experimental scripts.

--- scripts/expz5-5.py
+++ scripts/expz5-5.py
@@ -2,27 +2,39 @@
 
 import os
 
+expname = "ddos5z5"
+
+t_len = 512
+
 phases = []
-phases.append({"start" : 1,     "length" : 512.0, "alarms" : 0.0})  # phase 0: training
-phases.append({"start" : 513,   "length" : 256.0, "alarms" : 0.0})  # phase 1: detection under safety
-phases.append({"start" : 769,   "length" : 512.0, "alarms" : 0.0})  # phase 2: detection under attack
-phases.append({"start" : 1281,  "length" : 256.0, "alarms" : 0.0})  # phase 3: detection under safety
+phases.append({"start" : 1,             "length" : t_len,   "alarms" : 0.0})  # phase 0: training
+phases.append({"start" : 1+t_len,       "length" : t_len/2, "alarms" : 0.0})  # phase 1: detection under safety
+phases.append({"start" : 1+3*t_len/2,   "length" : t_len,   "alarms" : 0.0})  # phase 2: detection under attack
+phases.append({"start" : 1+5*t_len/2,   "length" : t_len/2, "alarms" : 0.0})  # phase 3: detection under safety
+
+#print(phases)
+
+#quit()
 
 kvalues = []
 
-for i in range(40,5,-1):    # This goes from 5.00 down to 0.75
+for i in range(32,5,-1):    # This goes from 4.00 down to 0.75
     kvalues.append(i/8.0)
 
+paths = {}
+
+paths["ee_bin"] = "/media/p4/ddosd-cpp/bin"
+paths["ee_json"] = "/media/p4/ddosd-p4/scripts"
+paths["ee_pcap"] = "/media/p4/ddos/datasets/zed"
+paths["tcad_bin"]= "/media/p4/ddosd-cpp/bin"
+paths["working_dir"] = "/media/p4"
 
 for kvalue in kvalues:
-    filename="ddos5z5-{:.3f}.tcad.txt".format(kvalue)
-    command="./ddosd-cpp/bin/ee -c ./ddosd-cpp/example/ddos5z1.json ./ddos/datasets/zed/zed20percent.pcap | ./ddosd-cpp/bin/tcad -t 512 -s 0.078125 -k {:f} > {}".format(kvalue,filename)
-    os.system(command)
-
-# quit()
-
-for kvalue in kvalues:
-    filename="ddos5z5-{:.3f}.tcad.txt".format(kvalue)
+    filename="{}/{}-{:.3f}.tcad.txt".format(paths["working_dir"],expname,kvalue)
+    command="{}/ee -c {}/{}.json {}/zed20percent.pcap | {}/tcad -t {} -s 0.078125 -k {:f} > {}".\
+        format(paths["ee_bin"],paths["ee_json"],expname,paths["ee_pcap"],paths["tcad_bin"],t_len,kvalue,filename)
+    #print(command)
+    #os.system(command)
     with open(filename) as f:
         for phase in phases:
             phase["alarms"] = 0
@@ -47,5 +59,8 @@ for kvalue in kvalues:
     tpr=phases[2]["alarms"]/phases[2]["length"]
     fpr=(phases[1]["alarms"] + phases[3]["alarms"])/phases[2]["length"]
     print("{:.3f} {:6f} {:6f} {:6f} {:6f}".format(kvalue, phases[1]["alarms"]/phases[1]["length"], tpr, phases[3]["alarms"]/phases[3]["length"], fpr))
+
+
+
 
 
