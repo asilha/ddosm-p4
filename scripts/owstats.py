de374b59f23bb5fe981635156f5308f09476ebe5
Alexandre Ilha <44216548+asilha@users.noreply.github.com>
2019-08-24 13:56:26 -0300

    Minor update.

--- scripts/owstats.py
+++ scripts/owstats.py
@@ -7,17 +7,31 @@ class PCAPSample:
     ow_first: int = 0
     ow_last:  int = 0
     ow_size:  int = 0
-    pcap_path: str = ""
-    pcap_name: str = ""
+    base_path: str = ""
+    base_name: str = ""
 
-def get_editcap_command(pcap_path, pcap_name, ow, packet_first, packet_last):
+def get_base_path_name(base_path, base_name):
+    return base_path + "/" + base_name
+
+def get_input_name(base_path_name):
+    return base_path_name
+
+def get_output_name(base_path_name, ow, packet_first, packet_last):
+    return base_path_name + "-" + str(ow) + "-" + str(packet_first) + "-" + str(packet_last)
+
+def get_editcap_command(input_name, output_name, packet_first, packet_last):
     
-    pcap_in_path_name = pcap_path+"/"+pcap_name+".pcap"
-    assert os.path.isfile(pcap_in_path_name)
-    pcap_out_path_name = pcap_path + "/" + pcap_name + "-" + str(ow) + "-" + str(packet_first) + "-" + str(packet_last) + ".pcap"
-    assert not os.path.isfile(pcap_out_path_name)
+    assert os.path.isfile(input_name + ".pcap")
+    # assert not os.path.isfile(output_name + ".pcap")
 
-    return "editcap -r " + pcap_in_path_name + " " + pcap_out_path_name + " " + str(packet_first) + "-" + str(packet_last)
+    return "editcap -r " + input_name + ".pcap " + output_name + ".pcap " + str(packet_first) + "-" + str(packet_last)
+
+def get_tshark_command(output_name):
+
+    assert os.path.isfile(output_name + ".pcap")
+    # assert not os.path.isfile(output_name + ".csv")
+
+    return "tshark -r " + output_name + ".pcap -T fields -e ip.src -e ip.dst -e data.data -E separator=, > " + output_name + ".csv"
 
 def main():
 
@@ -25,21 +39,30 @@ def main():
     sample.ow_first = 189
     sample.ow_last = 196
     sample.ow_size = 8192
-    sample.pcap_path = "D:/downloads/p4/ddos/datasets/zed"
-    sample.pcap_name = "zed20percent"
+    sample.base_path = "D:/downloads/p4/ddos/datasets/zed"
+    sample.base_name = "zed20percent"
 
     assert sample.ow_first > 0
     assert sample.ow_last > 0
     assert sample.ow_last >= sample.ow_first
     assert sample.ow_size > 0
 
-    for i in range(sample.ow_first, sample.ow_last):
-        packet_first = sample.ow_size * (i - 1) + 1  
+    for ow in range(sample.ow_first, sample.ow_last):
+        packet_first = sample.ow_size * (ow - 1) + 1  
         packet_last  = packet_first + sample.ow_size - 1
         assert packet_last - packet_first + 1 == sample.ow_size
-        command = get_editcap_command(sample.pcap_path, sample.pcap_name, i, packet_first, packet_last)
-        # print(command)
-        os.system(command)    
+        
+        base_path_name = get_base_path_name(sample.base_path, sample.base_name)
+        input_name = get_input_name(base_path_name)
+        output_name = get_output_name(base_path_name, ow, packet_first, packet_last)
+        
+        command = get_editcap_command(input_name, output_name, packet_first, packet_last)
+        print(command)
+        os.system(command)
+        
+        command = get_tshark_command(output_name) 
+        print(command)
+        os.system(command)
 
 if __name__ == '__main__':
     main()
