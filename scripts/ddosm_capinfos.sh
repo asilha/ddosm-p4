c6c894245a61dc847d91013bc5d8ecc42b42b85e
Alexandre Ilha <44216548+asilha@users.noreply.github.com>
2019-11-07 09:27:06 -0300

    Updated capinfos script

--- scripts/ddosm_capinfos.sh
+++ scripts/ddosm_capinfos.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
-export DIR=../pcaps/ddos20
-export OPTIONS="-c -M"
+#set -xe
 
-capinfos $OPTIONS $DIR/if2_legitimate_out.pcap
-capinfos $OPTIONS $DIR/if3_attack_out.pcap
-capinfos $OPTIONS $DIR/if4_stats_out.pcap
+DIR=$1
+OPTIONS="-c -M -T -r"
+INFO_CMD="capinfos $OPTIONS"
+CUT_CMD="cut -f 2"
+
+N_GOOD=`$INFO_CMD $DIR/if2_legitimate_out.pcap | $CUT_CMD`
+N_EVIL=`$INFO_CMD $DIR/if3_attack_out.pcap | $CUT_CMD`
+N_STAT=`$INFO_CMD $DIR/if4_stats_out.pcap | $CUT_CMD`
+
+echo "Good: \t$N_GOOD"
+echo "Evil: \t$N_EVIL" 
+echo "Total: \t$(( N_GOOD + N_EVIL ))"
+echo "OWs: \t$N_STAT"
+
+echo 
