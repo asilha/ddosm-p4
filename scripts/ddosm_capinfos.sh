badc95fde844e1e1684ffb08cc6026fb0bb10717
Alexandre Ilha <44216548+asilha@users.noreply.github.com>
2019-11-07 13:26:53 -0300

    It worked, but a black hole sucked 99 packets for no reason.

--- scripts/ddosm_capinfos.sh
+++ scripts/ddosm_capinfos.sh
@@ -11,13 +11,24 @@ CUT_CMD="cut -f 2"
 #N_EVIL=`$INFO_CMD $DIR/if3_attack_out.pcap | $CUT_CMD`
 #N_STAT=`$INFO_CMD $DIR/if4_stats_out.pcap | $CUT_CMD`
 
+
+N_SENT=`$INFO_CMD $DIR/veth0.pcap.gz | $CUT_CMD`
 N_GOOD=`$INFO_CMD $DIR/veth2.pcap.gz | $CUT_CMD`
 N_EVIL=`$INFO_CMD $DIR/veth4.pcap.gz | $CUT_CMD`
 N_STAT=`$INFO_CMD $DIR/veth6.pcap.gz | $CUT_CMD`
 
+echo "--------------------"
+echo "Input: \t$N_SENT"
+
+echo "--------------------"
 echo "Good: \t$N_GOOD"
 echo "Evil: \t$N_EVIL" 
 echo "Total: \t$(( N_GOOD + N_EVIL ))"
+
+echo "--------------------"
+echo "Lost: \t$(( N_SENT - N_GOOD - N_EVIL ))"
+
+echo "--------------------"
 echo "OWs: \t$N_STAT"
 
-echo 
+
