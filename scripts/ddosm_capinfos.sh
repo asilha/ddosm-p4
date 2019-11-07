b8e928357c347d4213f1f939d61399af25ccd9fc
Alexandre Ilha <44216548+asilha@users.noreply.github.com>
2019-11-07 11:20:15 -0300

    It works!
    Former-commit-id: 45eebe01ff1aeb1a1086a82ce7fe6cb9a7a497a3

--- scripts/ddosm_capinfos.sh
+++ scripts/ddosm_capinfos.sh
@@ -7,9 +7,13 @@ OPTIONS="-c -M -T -r"
 INFO_CMD="capinfos $OPTIONS"
 CUT_CMD="cut -f 2"
 
-N_GOOD=`$INFO_CMD $DIR/if2_legitimate_out.pcap | $CUT_CMD`
-N_EVIL=`$INFO_CMD $DIR/if3_attack_out.pcap | $CUT_CMD`
-N_STAT=`$INFO_CMD $DIR/if4_stats_out.pcap | $CUT_CMD`
+#N_GOOD=`$INFO_CMD $DIR/if2_legitimate_out.pcap | $CUT_CMD`
+#N_EVIL=`$INFO_CMD $DIR/if3_attack_out.pcap | $CUT_CMD`
+#N_STAT=`$INFO_CMD $DIR/if4_stats_out.pcap | $CUT_CMD`
+
+N_GOOD=`$INFO_CMD $DIR/veth2.pcap.gz | $CUT_CMD`
+N_EVIL=`$INFO_CMD $DIR/veth4.pcap.gz | $CUT_CMD`
+N_STAT=`$INFO_CMD $DIR/veth6.pcap.gz | $CUT_CMD`
 
 echo "Good: \t$N_GOOD"
 echo "Evil: \t$N_EVIL" 
