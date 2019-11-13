48d26f602d808035e011f945ea3dbae190a717f1
Alexandre Ilha <44216548+asilha@users.noreply.github.com>
2019-11-12 21:16:30 -0300

    Updated stats script
    Former-commit-id: 22df1ac3024c966743d9e923867b250810571103

--- scripts/p4d_stats.py
+++ scripts/p4d_stats.py
@@ -12,30 +12,22 @@ class PCAPSample:
     base_path: str = ""
     base_name: str = ""
 
-def get_output_name(base_path_name, ow, packet_first, packet_last):
-    return base_path_name + "-" + str(ow) + "-" + str(packet_first) + "-" + str(packet_last)
 
-def run_editcap(input_name, output_name, packet_first, packet_last):
-    assert os.path.isfile(input_name + ".pcapng")
-    command = "editcap -r " + input_name + ".pcapng " + output_name + ".pcap " + str(packet_first) + "-" + str(packet_last)
-    print(command)
+def run_tshark(base_path_name):
+    assert os.path.isfile(base_path_name + ".pcapng")
+    command = "echo src,dst,id,attack > " + base_path_name + ".csv"
     os.system(command)
-
-def run_tshark(output_name):
-    assert os.path.isfile(output_name + ".pcap")
-    command = "echo src,dst,attack > " + output_name + ".csv"
-    os.system(command)
-    command = "tshark -r " + output_name + ".pcap -T fields -e ip.src -e ip.dst -e ip.id -e data.data | " + \
-              "awk 'BEGIN {OFS=\",\"} {print $1,$2,$3,substr($4,50,1) }' >> " + output_name + ".csv"
-    # print(command)
+    command = "tshark -r " + base_path_name + ".pcapng -T fields -e ip.src -e ip.dst -e ip.id -e data.data | " + \
+              "awk 'BEGIN {OFS=\",\"} {print $1,$2,$3,substr($4,50,1) }' >> " + base_path_name + ".csv"
+    #print(command)
     os.system(command)
 
 def main():
     sample = PCAPSample()
     sample.ow_first = 1
-    sample.ow_last = 32
+    sample.ow_last = 1024
     sample.ow_size = 16384
-    sample.base_path = "~/p4sec/ddosm-p4/pcaps/ddos20m14"
+    sample.base_path = "/home/ilha/p4sec/ddosm-p4/pcaps/ddos20m14"
     sample.base_name = "if3_attack_out"
 
     assert sample.ow_first > 0
@@ -44,19 +36,8 @@ def main():
     assert sample.ow_size > 0
 
     base_path_name = sample.base_path + "/" + sample.base_name
-    input_name = base_path_name
-
-    for ow in range(sample.ow_first, sample.ow_last):
         
-        packet_first = sample.ow_size * (ow - 1) + 1  
-        packet_last  = packet_first + sample.ow_size - 1
-        assert packet_last - packet_first + 1 == sample.ow_size
-        
-        output_name = get_output_name(base_path_name, ow, packet_first, packet_last)
-        
-        run_editcap(input_name, output_name, packet_first, packet_last)
-        
-        run_tshark(output_name) 
+    run_tshark(base_path_name) 
         
 if __name__ == '__main__':
     main()
