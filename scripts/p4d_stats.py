2b0bb14067e99197b43d2e3ad1094964668cf537
Alexandre Ilha <44216548+asilha@users.noreply.github.com>
2019-11-25 11:30:47 -0300

    Updated analysis script

--- scripts/p4d_stats.py
+++ scripts/p4d_stats.py
@@ -15,11 +15,14 @@ class PCAPSample:
 
 def run_tshark(base_path_name):
     assert os.path.isfile(base_path_name + ".pcapng")
-    command = "echo src,dst,id,attack > " + base_path_name + ".csv"
+    # command = "echo src,dst,id,attack > " + base_path_name + ".csv"
+    command = "echo src,dst,src_delta,dst_delta,attack > " + base_path_name + ".csv"
     print(command)
     #os.system(command)
-    command = "tshark -r " + base_path_name + ".pcapng -T fields -e ip.src -e ip.dst -e ip.id -e data.data | " + \
-              "awk 'BEGIN {OFS=\",\"} {print $1,$2,$3,substr($4,50,1) }' >> " + base_path_name + ".csv"
+    # command = "tshark -r " + base_path_name + ".pcapng -T fields -e ip.src -e ip.dst -e ip.id -e data.data | " + \
+    #           "awk 'BEGIN {OFS=\",\"} {print $1,$2,$3,substr($4,50,1) }' >> " + base_path_name + ".csv"
+    command = "tshark -r " + base_path_name + ".pcapng -T fields -e ip.src -e ip.dst -e ip.id -e ip.checksum -e data.data | " + \
+              "awk 'BEGIN {OFS=\",\"} {print $1,$2,$3,$4,substr($5,50,1) }' >> " + base_path_name + ".csv"
     print(command)
     #os.system(command)
 
@@ -28,7 +31,7 @@ def main():
     sample.ow_first = 1
     sample.ow_last = 1024
     sample.ow_size = 16384
-    sample.base_path = "/home/ilha/p4sec/ddosm-p4/pcaps/ddos20m14"
+    sample.base_path = "/home/ilha/p4sec/ddosm-p4/pcaps/ddos20m14b"
     sample.base_name = "if3_attack_out"
 
     assert sample.ow_first > 0
