ded1d06d68e499169119bacfa96e4fb2b85642c8
Alexandre Ilha <44216548+asilha@users.noreply.github.com>
2020-01-02 08:00:10 -0300

    Advancing...

--- scripts/run_without_config.sh
+++ scripts/run_without_config.sh
@@ -12,13 +12,13 @@ sigint_handler() {
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-#SS_PREFIX="/home/p4/p4sec/aclapolli-bmv2/targets/simple_switch"
 SS_PREFIX="/usr/local/bin"
 
-SS_PARAMS="--log-level off"
+#SS_PARAMS="--log-level off"
+SS_PARAMS="--log-file debug --log-level debug"
 
 trap sigint_handler SIGINT
-$SS_PREFIX/simple_switch -i 1@veth0 -i 2@veth2 -i 3@veth4 -i 4@veth6 $SS_PARAMS $SCRIPT_DIR/../build/ddosm.json &
+$SS_PREFIX/simple_switch -i 1@veth1 -i 2@veth3 -i 3@veth5 -i 4@veth7 $SS_PARAMS $SCRIPT_DIR/../build/ddosm.json &
 pid=$!
 sleep 5
 echo Switch is running. use CTRL+C to interrupt.
