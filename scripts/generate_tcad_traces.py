b8190c995dffcb9589e966daf776bfb35dce9b14
Alexandre Ilha <44216548+asilha@users.noreply.github.com>
2020-01-04 22:15:39 -0300

    Updated generate_tcad_traces.py
    Former-commit-id: d779d439f175c1f0e10667b41d55dd8ab118110b
    Former-commit-id: 2bcbac23f8d9ea8e2f68b5d66329b9600646e7f6

--- scripts/generate_tcad_traces.py
+++ scripts/generate_tcad_traces.py
@@ -1,46 +1,45 @@
 #!/usr/bin/python3
 
+import argparse
 import os
 
-kvalues = []
-for i in range(0,65):    # This goes from 0.000 to 8.000
-    kvalues.append(i/8.0)
+def main():
 
-mvalues = []
-for i in range(18,19):  # This goes from 18 to 18.
-    mvalues.append(i)
+    parser = argparse.ArgumentParser(description="Submits ee traces to tcad using several levels of k.")
+    parser.add_argument("-m", "--log2_m", help="Binary logarithm of OW length")
+    parser.add_argument("-t", "--training_length", help="Training length")
+    parser.add_argument("-i", "--input_log_file", help="File with ee traces")
+    parser.add_argument("-o", "--output_log_path", help="Directory for tcad trace output")
+    args = parser.parse_args()
 
-# training_packets_log2_n = 23
+    print(args)
 
-paths = {}
-paths["tcad_bin"]= "~/p4sec/ddosd-cpp/bin"
-paths["ee_logs"] = "~/p4sec/ddosm-p4/lab/ddos20-full/ee_logs"
-paths["tcad_logs"] = "/home/ilha/p4sec/ddosm-p4/lab/ddos20-full/tcad_logs"
+    log2_m = int(args.log2_m)
+    training_length = int(args.training_length)
+    input_log_file = args.input_log_file
+    output_log_path = args.output_log_path
+    summary_log = output_log_path + "/summary.log"   
 
-summary_log = paths["tcad_logs"]+"/summary.log"   
+    tcad_bin = "~/p4sec/ddosd-cpp/bin/tcad"
 
-with open(summary_log, "w") as f:
-    f.write("log2m,k,n,t,p1len,p1alm,p2len,p2alm,p3len,p3alm,tpr,fpr,fpr1,fpr3\n")
+    with open(summary_log, "w") as f:
+        f.write("log2m,k,n,t,p1len,p1alm,p2len,p2alm,p3len,p3alm,tpr,fpr,fpr1,fpr3\n")
 
-for mvalue in mvalues:
-    ee_log = paths["ee_logs"]+"/ee_m_2_"+str(mvalue)+".log"
-    # print(ee_log)
-     
-    training_length = 250  # 2 ** (training_packets_log2_n - mvalue)
+    kvalues = []
+    for i in range(0,65):    # This goes from 0.000 to 8.000
+        kvalues.append(i/8.0)
+
     for kvalue in kvalues:
-        tcad_log = paths["tcad_logs"]+"/tcad_m_2_"+str(mvalue)+"_k_"+ "{:.3f}".format(kvalue) +".log"
-        # print(tcad_log) 
-        tcad_command = paths["tcad_bin"] + "/tcad -t " + str(training_length) + " -s 0.078125 -k " + str(kvalue) 
-        # print(tcad_command)
-        command = "cat " + ee_log + " | " + tcad_command + " > " + tcad_log
+        tcad_log = output_log_path + "/tcad_m_2_" + log2_m + "_k_" + "{:.3f}".format(kvalue) +".log"
+        tcad_command = tcad_bin + " -t " + str(training_length) + " -s 0.078125 -k " + str(kvalue) 
+        command = "cat " + input_log_file + " | " + tcad_command + " > " + tcad_log
         print(command)
-        os.system(command)
+        # os.system(command)
         phases = []
         phases.append({"start" : 1,                       "length" : training_length,   "alarms" : 0.0})  # phase 0: training
         phases.append({"start" : 1+training_length,       "length" : training_length/2, "alarms" : 0.0})  # phase 1: detection under safety
         phases.append({"start" : 1+3*training_length/2,   "length" : training_length,   "alarms" : 0.0})  # phase 2: detection under attack
         phases.append({"start" : 1+5*training_length/2,   "length" : training_length/2, "alarms" : 0.0})  # phase 3: detection under safety
-        # print(phases)    
         with open(tcad_log) as f:
             for phase in phases:
                 phase["alarms"] = 0
@@ -66,15 +65,16 @@ for mvalue in mvalues:
             fpr3=phases[3]["alarms"]/phases[3]["length"]
             fpr=(phases[1]["alarms"] + phases[3]["alarms"])/phases[2]["length"]
             with open(summary_log,"a") as f:
-              # f.write("log2m,k,n,t,p1len,p1alm,p2len,p2alm,p3len,p3alm,tpr,fpr,fpr1,fpr3\n")
                 f.write("{},{:.3f},{},{},{},{},{},{},{},{},{:.3f},{:.3f},{:.3f},{:.3f}\n".format(
-                    mvalue,kvalue,count,training_length,
+                    log2_m,kvalue,count,training_length,
                     phases[1]["length"],phases[1]["alarms"],
                     phases[2]["length"],phases[2]["alarms"],
                     phases[3]["length"],phases[3]["alarms"],
                     tpr,fpr,fpr1,fpr3))
 
 
+if __name__ == '__main__':
+    main()
 
 
 
